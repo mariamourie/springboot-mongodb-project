1a261f72be6b3382c332c94235f14b0c6f064573
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-09 14:41:47 -0300

    Project created

---
+++ src/main/java/com/mariamourie/workshopmongo/SpringbootMongodbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mariamourie.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariamourie/workshopmongo/SpringbootMongodbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariamourie.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
