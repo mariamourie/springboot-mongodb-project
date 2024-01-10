5318ef0864e66bd77db5f61666a791ea5f5efb96
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-10 18:41:26 -0300

    Updating user data with the PUT method

--- src/main/java/com/mariamourie/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mariamourie/workshopmongo/resources/UserResource.java
@@ -53,4 +53,12 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		User user = service.fromDTO(objDTO);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/mariamourie/workshopmongo/services/UserService.java
+++ src/main/java/com/mariamourie/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());		
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
