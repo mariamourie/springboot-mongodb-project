73d819ab6012296844e99017869cbb166eb506f5
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-11 13:48:18 -0300

    Getting a post by an id

---
+++ src/main/java/com/mariamourie/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mariamourie.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariamourie.workshopmongo.domain.Post;
+import com.mariamourie.workshopmongo.repository.PostRepository;
+import com.mariamourie.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
