532bb69779779713e62a0db97647c16baf53b6d3
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-11 13:09:07 -0300

    Creating the Post entity with an aligned User

---
+++ src/main/java/com/mariamourie/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.mariamourie.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mariamourie.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
