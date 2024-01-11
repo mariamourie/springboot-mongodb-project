578a1bbdf0013188c6a52b2b70c52e66f439c2b2
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-11 13:18:38 -0300

    Projecting author data with DTO

--- src/main/java/com/mariamourie/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mariamourie/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mariamourie.workshopmongo.domain.Post;
 import com.mariamourie.workshopmongo.domain.User;
+import com.mariamourie.workshopmongo.dto.AuthorDTO;
 import com.mariamourie.workshopmongo.repository.PostRepository;
 import com.mariamourie.workshopmongo.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
